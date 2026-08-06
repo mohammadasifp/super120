6fb1b5c95da815befeb7dcbe095fc9ce3a67c7ba
moham <moham@example.com>
2026-08-06 10:49:03 +0530

    Auto-commit: "Day 8/StackLL.py" at 8/6/2026, 10:49:03 AM

---
+++ Day 8/StackLL.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+    
+class StackLL:
+    def __init__(self):
+        self.head=None
+        
+    def push(self,data):
+        newNode=Node(data)
+        newNode.next=self.head
+        self.head=newNode
+        
+    def pop(self):
+        if self.head is None:
+            print("Stack is Empty")
+            return
+
+        value = self.head.data
+        self.head = self.head.next
+        return value
+    
+    def peek(self):
+        if self.head is None:
+            print("Stack is Empty")
+            return
+        print("Satck top element is ",self.head.data)
+    
+    def display(self):
+        if self.head is None:
+            print("Stack is empty")
+            return
+        print("Stack elemts are :")
+        temp=self.head
+        while temp != None:
+            print(temp.data ,end=" -> ")
+            temp=temp.next
+            
+s=StackLL()
+n=int(input("Enter the number of elements you want to push in stack :"))
+for i in range(n):
+    item=int(input("Enter the value :"))
+    s.push(item)
+s.display()
+s.pop()
+s.peek()
+s.display()    
